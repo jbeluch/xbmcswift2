64c75f5684431a77e2e6dc95e3654169d3330bb0
Jonathan Beluch <jon@jonathanbeluch.com>
2012-09-09 14:47:22 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,48 @@
+'''
+
+xbmcswift2
+~~~~~~~~~~
+
+A micro framework to enable rapid development of XBMC plugins.
+
+
+Features
+````````
+
+* Run the addon from the command line *or* within XBMC without changing any
+  code.
+* Helper libraries to make common XBMC api operations easy, like adding items,
+  getting settings, creating temporary files, etc.
+* Handles all the url parsing involved in plugin routing. No need to deal with
+  complicated URLs and query strings.
+
+
+Documentation
+`````````````
+
+The current documentation can be found at http://www.xbmcswift.com
+
+Development
+```````````
+
+This module is now available in the official XBMC Eden repository as
+xbmcswift2.
+
+This project is the next version of xbmcswift. While the APIs are similar,
+there are a few things that are not backwards compatible with the original
+version, hence the new name.
+
+
+Contact
+```````
+
+https://github.com/jbeluch/xbmcswift2
+
+xbmc@jonathanbeluch.com
+'''
 import os
 from setuptools import setup, find_packages
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name = 'xbmcswift2',
@@ -16,30 +52,22 @@ setup(
     description = 'A micro framework for rapid development of XBMC plugins.',
     license = "GPL3",
     keywords = "example documentation tutorial",
-    #url = '',
-    #packages=['xbmcswift'],
-    #package_dir={'xbmcswift': 'xbmcswift'},
+    url = 'https://github.com/jbeluch/xbmcswift2',
     package_data={'xbmcswift2': [
-        #skel/
+        'README.md',
         'cli/data/addon.xml',
         'cli/data/addon.py',
-        #'skel/resources',
-        #'skel/resources/lib',
         'cli/data/resources/lib/__init__.py',
         'cli/data/resources/__init__.py',
-        #'skel/resources/language',
-        #'skel/resources/language/English',
         'cli/data/resources/language/English/strings.xml',
     ]},
 
     packages=find_packages(),
-    #packages=['xbmcswift2'],
     include_package_data=True,
-    long_description=read('README.md'),
+    long_description=__doc__,
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
-        #"License :: OSI Approved :: BSD License",
         'License :: OSI Approved :: GNU Affero General Public License v3',
         'Programming Language :: Python',
     ],
