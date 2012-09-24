b6e26ef99a21ae97948f9791d127c782fc764627
Jonathan Beluch <jon@jonathanbeluch.com>
2012-09-23 21:58:17 -0600

    Fix #9. Fix #4. Update skeleton.
    Fixes the default plugin created when using `xbmcswift2 create`.

--- xbmcswift2/cli/data/addon.py
+++ xbmcswift2/cli/data/addon.py
@@ -1,12 +1,16 @@
 from xbmcswift2 import Plugin
 
 
-plugin = Plugin(PLUGIN_NAME, PLUGIN_ID, __file__)
+plugin = Plugin()
 
 
 @plugin.route('/')
 def index():
-    item = {'label': 'Hello XBMC!'}
+    item = {
+        'label': 'Hello XBMC!',
+        'path': 'http://s3.amazonaws.com/KA-youtube-converted/JwO_25S_eWE.mp4/JwO_25S_eWE.mp4',
+        'is_playable': True
+    }
     return [item]
 
 
